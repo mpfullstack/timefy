342928a7e1a682e5a417f46ff66e37efa7253c41
mpfullstack <info@marcperez.cat>
2018-05-27 17:45:30 +0200

    getClients API and saga implementation

--- src/redux/globalSagas.js
+++ src/redux/globalSagas.js
@@ -3,33 +3,24 @@
  * @Date:   2018-05-25T22:09:32+02:00
  * @Email:  info@marcperez.cat
  * @Last modified by:   welvi
- * @Last modified time: 2018-05-25T22:28:09+02:00
+ * @Last modified time: 2018-05-27T17:41:07+02:00
  */
 
 
-import { all, takeLatest, put, fork } from 'redux-saga/effects';
+import { all, takeLatest, put, call, fork } from 'redux-saga/effects';
 import globalActions from './globalActions';
-
-const clients = [
-  {
-    "id": 1,
-    "name": "Whads Accent SL"
-  },
-  {
-    "id": 2,
-    "name": "Cocuyo Labs"
-  },
-  {
-    "id": 3,
-    "name": "SERHS Tourism"
-  }
-];
+import { getClients } from '../helpers/api';
 
 export function* onRequestItems() {
   yield takeLatest(globalActions.REQUEST_ITEMS, function*({ itemType }) {
+    let fetchItems;
+    if( itemType === 'clients' ) {
+      fetchItems = getClients;
+    }
+    const result = yield call(fetchItems);
     yield put({
       type: globalActions.REQUEST_ITEMS_SUCCESS,
-      items: clients
+      items: result
     });
   });
 }
