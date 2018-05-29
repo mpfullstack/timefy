c908f96037127fbb9151728dba2532e962da6ca9
mpfullstack <info@marcperez.cat>
2018-05-29 20:06:18 +0200

    Defined API urls for projects and tasks

--- src/config.js
+++ src/config.js
@@ -3,15 +3,19 @@
  * @Date:   2018-02-20T16:40:49+01:00
  * @Email:  info@marcperez.cat
  * @Last modified by:   welvi
- * @Last modified time: 2018-05-27T17:35:26+02:00
+ * @Last modified time: 2018-05-29T17:43:54+02:00
  */
 
 const baseUrl = 'http://localhost:3001/';
 const baseAPIUrl = `${baseUrl}api`;
 const clientsUrl = `${baseAPIUrl}/clients`;
+const projectsUrl = `${baseAPIUrl}/projects`;
+const tasksUrl = `${baseAPIUrl}/tasks`;
 
 const apiUrl = {
-  getClients: clientsUrl
+  getClients: clientsUrl,
+  getProjects: projectsUrl,
+  getTasks: tasksUrl
 };
 
 export {
